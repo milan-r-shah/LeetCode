dbc07e1c416ffd23b0c8414651b4123b5ff5f1ae
Milan Shah <mrshah@wpi.edu>
2020-05-24 03:23:58 -0400

    Added 1_Two_Sum.cpp file

---
+++ LeetCode_cpp/1_Two_Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::vector<int> indices_vec;
+        for(int i = 0; i < nums.size(); ++i) {
+            for(int j = i+1; j < nums.size(); ++j) {
+                if (nums[i] == target - nums[j]) {
+                    indices_vec.push_back(i);
+                    indices_vec.push_back(j);
+                }
+            }
+        }
+        return indices_vec;
+    }
+};
