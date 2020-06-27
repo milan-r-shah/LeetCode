f93dc6172ce53b846d973bbd3b0749db6d66d36e
Milan Shah <mrshah@wpi.edu>
2020-06-26 20:24:32 -0400

    1385. Find the Distance Value Between Two Arrays | Array
    Added 2 solutions for problem 1385
    Sol 01: brute force
    Sol 02: brute force -  little bit simpler

--- 1385_Find_the_Distance_Value_Between_Two_Arrays/sol02_brute_force_simpler.cpp
+++ 1385_Find_the_Distance_Value_Between_Two_Arrays/sol02_brute_force_simpler.cpp
@@ -4,7 +4,7 @@ https://leetcode.com/problems/find-the-distance-value-between-two-arrays/
 */
 
 //
-// Solution 01: Brute force
+// Solution 02: Brute force
 //
 // Runtime: ~20 ms   (faster than 71.81% of C++ online submissions)
 // Memory:  ~13.2 MB (less than 39.99% of C++ online submissions)
@@ -16,21 +16,15 @@ public:
         int size1 = arr1.size();
         int size2 = arr2.size();
 
-        int ans = 0;
-        int cnt2 = 0;
+        int ans = size1;
 
-        for (int i = 0; i < size1; ++i) {
-            for (int j = 0; j < size2; ++j) {
-                if (abs(arr1[i] - arr2[j]) <= d)
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
+                if (abs(arr1[i] - arr2[j]) <= d) {
+                    ans--;
                     break;
-                else
-                    cnt2++;
+                }
             }
-
-            if (cnt2 == size2)
-                ans++;
-
-            cnt2 = 0;
         }
 
         return ans;
